64eecc820434037dd90ae3046eb991d33cd03d0c
Nate Yonkee <tulanthoar@gmail.com>
2016-06-10 16:39:46 -0600

    add more interplay between xmonad and bytmux

--- dotfiles/xmonad/xmonad.hs
+++ dotfiles/xmonad/xmonad.hs
@@ -1,48 +1,15 @@
--- Then edit your handleEventHook by adding the appropriate event hook from below
-
---  serverModeEventHook :: Event -> X All
-
---  Executes a command of the list when receiving its index via a special ClientMessageEvent (indexing starts at 1). Sending index 0 will ask xmonad to print the list of command numbers in stderr (so that you can read it in ~/.xsession-errors). Uses XMonad.Actions.
---  Commands as the default.
-
---  main = xmonad def { handleEventHook = }
---  xmonadctl 0 # tells xmonad to output command list
---  xmonadctl 1 # tells xmonad to switch to workspace 1
---  serverModeEventHook' :: X [(String, X ())] -> Event -> X All
-
---  serverModeEventHook' additionally takes an action to generate the list of commands.
-
---  serverModeEventHookCmd :: Event -> X All
-
---  Executes a command of the list when receiving its name via a special ClientMessageEvent. Uses XMonad.Actions.Commands as the default.
-
---  main = xmonad def { handleEventHook = }
---  xmonadctl run # Tells xmonad to generate a run prompt
---  serverModeEventHookCmd' :: X [(String, X ())] -> Event -> X All
-
---  Additionally takes an action to generate the list of commands
-
---  serverModeEventHookF :: String -> (String -> X ()) -> Event -> X All
-
---  Listens for an atom, then executes a callback function whenever it hears it. A trivial example that prints everything supplied to it on xmonad's standard out:
-
---  main = xmonad def { handleEventHook = }
---  xmonadctl -a XMONAD_PRINT "hello world"
--- import Data.Maybe
 import Control.Monad
-import Data.Maybe
+import Data.Maybe(fromMaybe)
 import qualified Data.Map as M
-import qualified XMonad.StackSet as W
-import qualified XMonad.Util.Loggers as L
-import System.Exit
 import System.IO(hPutStrLn)
 import XMonad
+import XMonad.ManageHook
+import XMonad.Prompt
+import qualified XMonad.StackSet as W
 import XMonad.Actions.Commands
 import XMonad.Actions.CycleWS
 import XMonad.Actions.FocusNth
 import XMonad.Actions.ShowText
-import XMonad.Actions.SpawnOn
-import XMonad.Actions.Submap
 import XMonad.Actions.UpdatePointer
 import XMonad.Actions.WithAll
 import XMonad.Hooks.DynamicLog
@@ -52,18 +19,17 @@ import XMonad.Hooks.Minimize
 import XMonad.Hooks.ServerMode
 import XMonad.Hooks.SetWMName
 import XMonad.Layout.BoringWindows
+import XMonad.Layout.Gaps
 import XMonad.Layout.Grid
 import XMonad.Layout.Magnifier
 import XMonad.Layout.Minimize
 import XMonad.Layout.NoBorders
 import XMonad.Layout.Reflect
 import XMonad.Layout.ToggleLayouts
-import XMonad.Layout.TwoPane
-import XMonad.ManageHook
-import XMonad.Prompt
 import XMonad.Prompt.AppLauncher
 import XMonad.Util.Dmenu
 import XMonad.Util.EZConfig(additionalKeysP, additionalKeys)
+import qualified XMonad.Util.Loggers as L
 import XMonad.Util.NamedScratchpad
 import XMonad.Util.Paste
 import XMonad.Util.Run(spawnPipe)
@@ -84,7 +50,6 @@ xmConf p = def
   , workspaces         = map (\w -> "<"++w++">") ["W", "d", "t", "T"]
   , logHook            = updatePointer (0.5,0.5) (0.5,0.5) >> dynamicLogWithPP ( myDzenPP p )
   , handleEventHook = docksEventHook <+> minimizeEventHook <+> handleTimerEvent <+> serverModeEventHook <+> serverModeEventHookCmd' xmC
-  -- <+> serverModeEventHookF "XMONAD_PRINT" (\s->spawn$ "xmessage " ++ s)
   , keys = \c -> M.fromList xkM
   }
 
@@ -94,23 +59,22 @@ main = do
   xmonad $ xmConf dzP
     `additionalKeysP` myKeysP
 
-mylayoutHook = avoidStrutsOn [U] $ boringWindows $ minimize $ magnifiercz 1.2 $ smartBorders $
-  toggleLayouts Grid $ Mirror $ Tall 1 delta ratio
+mylayoutHook = gaps [(U,18)] $ avoidStrutsOn [U] $ smartBorders $ magnifiercz 1.2 $
+  boringWindows $ minimize $ toggleLayouts Grid $ Mirror $ Tall 1 delta ratio
   where
     delta = 3 / 100
     ratio = 11 / 20
 
 -- Manage hook
-myManageHook = manageDocks
+myManageHook = namedScratchpadManageHook scratchpads
   <+> composeAll
   [ className =? "Xmessage"  --> doFloat
   ,className =? "Dialogue"  --> doFloat
   ]
   <+> (fmap not isDialog --> doF avoidMaster)
   <+> composeOne [ isFullscreen -?> doFullFloat ]
-  <+> namedScratchpadManageHook scratchpads
-  
-  -- , ((0, 122), sendKey noModMask xK_Down)
+  <+> manageDocks
+
 xkM=
   [ ((0, xK_Menu), launchAct)
   , ((0, raisevol), replicateM_ 5 $ sendKey noModMask xK_Down)
@@ -119,16 +83,17 @@ xkM=
   , ((myModMask, lowvol), moveTo Next NonEmptyWS >> avoidNSP)
   , ((myModMask, xK_F1), dmenuchoose $ M.fromList mC)
   , ((myModMask .|. controlMask, xK_y), defaultCommands >>= runCommand)
-  , ((noModMask, xK_Print), sendKey (controlMask .|. shiftMask) xK_F11 )
+  , ((0, xK_Print), sendKey controlMask xK_F10 )
   , ((myModMask, xK_Menu), srchAct)
   , ((myModMask, xK_y), sendKey noModMask xK_Return >> sendKey noModMask xK_F10)
   , ((myModMask, xK_v), sendKey controlMask xK_F11 >> sendKey noModMask xK_space)
-  -- ] ++
-  -- [ ((myModMask, k), focusNth i)
-  -- | (i, k) <- zip [0 .. 8] [xK_1 ..]
+  , ((0, 0xff9f), sendMessage ToggleLayout)
+  , ((0, 0xff9e), withFocused minimizeWindow)
+  , ((0, 0xffab), sendMessage RestoreNextMinimizedWin )
+  , ((0, 0xff8d), withFocused $ \w -> withAll minimizeWindow >> (sendMessage . RestoreMinimizedWin ) w )
   ] ++
-  [ ((noModMask, l), focusNth m)
-  | (m, l) <- zip [0 .. 8] [xK_KP_End, 0xff99, 0xff9b, 0xff96, 0xff9b , 0xff98, 0xff95, 0xff97, 0xff9a ]
+  [ ((0, l), focusNth m)
+  | (m, l) <- zip [0 .. 8] [xK_KP_End, 0xff99, 0xff9b, 0xff96, 0xff9d, 0xff98, 0xff95, 0xff97, 0xff9a ]
   ]
 
 raisevol=0x1008ff13
@@ -136,7 +101,6 @@ lowvol=0x1008ff11
 
 myKeysP =
   [ ("M-b", spawn myBrowser)
-  -- , ("M-j", focusedMin)
   , ("<Insert>", spawn "xdotool click 2")
   , ("M4-t",  spawn myTerminal )
   , ("M-c", spawn $ "clipmenu -z -l 50 -p 'clip' -fn "++apFnmenu)
