69993d4d04aa6de327b27ed62421e4097c9731ef
Nate <tulanthoar@gmail.com>
2016-11-26 16:33:40 -0700

    use io tied file handle

--- bin/start-pomodoro.pl
+++ bin/start-pomodoro.pl
@@ -1,20 +1,20 @@
 #!/bin/env perl
 use Modern::Perl('2015');
-use utf8;
 use IO::All;
 
-my $tmpfile = io '/tmp/pomodoro-status';
+my $iofile = io->file('/tmp/pomodoro-status')->assert->touch;
+my $iotie = $iofile->tie;
 my $bar = io->pipe(q/pymodoro -p '>' -b '_' -e '|' -l 40 -ltr -i 5/);
-while($tmpfile->is_writable){
+while($iofile->is_writable){
     io->file($ENV{HOME}.'/.pomodoro_session')->touch;
-    $tmpfile->[0] = $bar->getline;
+    $iotie->[0] = $bar->getline;
     my $pid = fork();
     if(not $pid){
         system("xmessage","pomodoro started", "-timeout", "5");
         exit;
     }
     until($bar->getline =~ /B/){
-        $tmpfile->[0] = $bar->getline;
+        $iotie->[0] = $bar->getline;
     }
     $pid = fork();
     if(not $pid){
@@ -22,6 +22,6 @@ while($tmpfile->is_writable){
         exit;
     }
     until($bar->getline =~ /B [0-9]{2}:[0-9]{2}/){
-        $tmpfile->[0] = $bar->getline;
+        $iotie->[0] = $bar->getline;
     }
 }
