7e965671eb541e9096bb97a98d51a70dbdfd9807
Nate <tulanthoar@gmail.com>
2016-11-30 23:43:42 -0700

    use proc::background module to avoid zombie processes in pomodoro

--- bin/start-pomodoro.pl
+++ bin/start-pomodoro.pl
@@ -1,6 +1,7 @@
 #!/bin/env perl
 use Modern::Perl('2015');
 use IO::All;
+use Proc::Background;
 
 my $iofile = io->file('/tmp/pomodoro-status')->assert->touch;
 my $iotie = $iofile->tie;
@@ -8,20 +9,14 @@ my $bar = io->pipe(q/pymodoro -p '>' -b '_' -e '|' -l 40 -ltr -i 5/);
 while($iofile->is_writable){
     io->file($ENV{HOME}.'/.pomodoro_session')->touch;
     $iotie->[0] = $bar->getline;
-    my $pid = fork();
-    if(not $pid){
-        system("xmessage","pomodoro started", "-timeout", "5");
-        exit;
-    }
+    my $p1 = Proc::Background->new("xmessage","pomodoro started", "-timeout", "5");
     until($bar->getline =~ /B/){
         $iotie->[0] = $bar->getline;
     }
-    $pid = fork();
-    if(not $pid){
-        system("xmessage","pomodoro break", "-timeout", "300");
-        exit;
-    }
+    my $p2 = Proc::Background->new("xmessage","break time", "-timeout", "300");
     until($bar->getline =~ /B [0-9]{2}:[0-9]{2}/){
         $iotie->[0] = $bar->getline;
     }
+    $p1->die;
+    $p2->die;
 }
